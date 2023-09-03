433b8d84415b53b5faad69501966f57a36dc6a5b
Numaan <manumaanowais809@gmail.com>
2023-09-03 12:01:23 +0530

    Updated items

--- src/Home.js
+++ src/Home.js
@@ -46,6 +46,18 @@ function Home({ gridItemsData }) {
             { label: "Prawns Mandi", price: "400" },
             { label: "Laham Mandi", price: "380" },
             { label: "Bbq Mandi", price: "410" },
+            { label: "Veg Mandi", price: "210" },
+            { label: "Chicken Fry Mandi", price: "300" },
+            { label: "Mutton Fry Mandi", price: "500" },
+            { label: "Fish Fry Mandi", price: "510" },
+            { label: "Prawns Fry Mandi", price: "610" },
+            { label: "Chicken Juicy Mandi", price: "260" },
+            { label: "Broasted Chicken Mandi", price: "300" },
+            { label: "Chicken 65 Mandi", price: "320" },
+            { label: "Grill Chicken Mandi", price: "350" },
+            { label: "Chicken Schezwan Mandi", price: "380" },
+            { label: "Chicken Special Mandi", price: "400" },
+            { label: "Chef Special Mandi", price: "500" }
 
         ],
         "biryani": [
@@ -67,6 +79,57 @@ function Home({ gridItemsData }) {
             { label: "Mutton Nahari", price: "150" },
             { label: "Mutton Masala", price: "130" },
             { label: "Mutton Tadaka", price: "120" }
+        ],
+        "prawns": [
+            { label: "Prawns Nahari", price: "150" },
+            { label: "Prawns Masala", price: "130" },
+            { label: "Prawns Tadaka", price: "120" }
+        ],
+        "egg": [
+            { label: "Egg Nahari", price: "150" },
+            { label: "Egg Masala", price: "130" },
+            { label: "Egg Tadaka", price: "120" }
+        ],
+        "veg": [
+            { label: "Veg Curry", price: "150" },
+            { label: "Veg Masala", price: "130" },
+            { label: "Veg Pulao", price: "120" }
+        ],
+        "maggie": [
+            { label: "Maggie Masala", price: "150" },
+            { label: "Maggie Fry", price: "130" },
+            { label: "Steamed Maggie", price: "120" }
+        ],
+        "meals": [
+            { label: "Dosa", price: "150" },
+            { label: "Plain Dosa", price: "130" },
+            { label: "Masala Dosa", price: "120" },
+            { label: "Idli", price: "110" },
+            { label: "Wada", price: "150" },
+            { label: "Bonda", price: "80" },
+            { label: "Mysore Bhajia", price: "100" },
+            { label: "Pakode", price: "110" },
+            { label: "Puri", price: "140" },
+        ],
+        "tea": [
+            { label: "Special Tea", price: "150" },
+            { label: "Masala Tea", price: "130" },
+            { label: "Kahak Tea", price: "120" }
+        ],
+        "coffee": [
+            { label: "Special Coffee", price: "150" },
+            { label: "Masala Coffee", price: "130" },
+            { label: "Kadak Coffee", price: "120" }
+        ],
+        "fastfood": [
+            { label: "Noodles", price: "150" },
+            { label: "Egg Rice", price: "130" },
+            { label: "Manchurian", price: "120" }
+        ],
+        "dessert": [
+            { label: "Fruit Salad", price: "150" },
+            { label: "Apricot Delight", price: "130" },
+            { label: "Bakalawa", price: "120" }
         ],
     };
 
@@ -169,27 +232,54 @@ function Home({ gridItemsData }) {
             <Header />
             <main className="Home-main">
                 <div className="section" style={{ width: '20%' }}>
-                    <h2>Menu Items</h2>
+                    <h4>Menu Items</h4>
                     <div id="section1">
                         <div className="section1">
                             <button className="btn" id="mandi" onClick={() => showSection2Content('mandi')}> Mandi </button>
-                        </div><br></br>
+                        </div>
                         <div className="section1">
                             <button className="btn" id="biryani" onClick={() => showSection2Content('biryani')}>Biryani</button>
-                        </div><br></br>
+                        </div>
                         <div className="section1">
                             <button className="btn" id="curry" onClick={() => showSection2Content('curry')}>Curries</button>
-                        </div><br></br>
+                        </div>
                         <div className="section1">
                             <button className="btn" id="fish" onClick={() => showSection2Content('fish')}>Fish</button>
-                        </div><br></br>
+                        </div>
                         <div className="section1">
                             <button className="btn" id="mutton" onClick={() => showSection2Content('mutton')}>Mutton</button>
-                        </div><br></br>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="prawns" onClick={() => showSection2Content('prawns')}>Prawns</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="egg" onClick={() => showSection2Content('egg')}>Egg</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="veg" onClick={() => showSection2Content('veg')}>Veg</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="maggie" onClick={() => showSection2Content('maggie')}>Maggie</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="meals" onClick={() => showSection2Content('meals')}>Meals</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="tea" onClick={() => showSection2Content('tea')}>Tea</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="coffee" onClick={() => showSection2Content('coffee')}>Coffee</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="fastfood" onClick={() => showSection2Content('fastfood')}>Fast Food</button>
+                        </div>
+                        <div className="section1">
+                            <button className="btn" id="dessert" onClick={() => showSection2Content('dessert')}>Dessert</button>
+                        </div>
                     </div>
                 </div>
                 <div className="section">
-                    <h2>Items</h2>
+                    <h4>Items</h4>
                     <div id="section2-content">
                         {section2Content.length > 0 ? (
                             <div className="section2-content-buttons">
@@ -208,7 +298,7 @@ function Home({ gridItemsData }) {
                     </div>
                 </div>
                 <div className="section">
-                    <h2>Billing Details</h2>
+                    <h4>Billing Details</h4>
                     <div id="section3-content">
                         {selectedButtons.length > 0 ? (
                             <table border={1} className='billing-table'>
